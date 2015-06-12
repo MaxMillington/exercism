e344261f26b3e23346db7289dd12ef4ae869d646
MaxMillington <maxmillington89@gmail.com>
2015-06-12 08:52:51 -0600

    robots beeeep boop beep beep beep

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,20 @@
+class Robot
+
+  def name
+    @name ||= "#{letters}#{numbers}"
+  end
+
+  def letters
+    ('A'..'Z').to_a.sample(2).join
+  end
+
+  def numbers
+    rand(100..999)
+  end
+
+  def reset
+    @name = nil
+  end
+
+
+end
