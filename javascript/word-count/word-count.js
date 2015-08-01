8dff0365dc8de785a46d700fbd8e8fb262f2e44d
MaxMillington <maxmillington89@gmail.com>
2015-08-01 09:13:54 -0600

    counting words in JS

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,10 @@
+module.exports = function (input) {
+    var counts = {};
+    var words = input.match(/\S+/g);
+
+    words.forEach(function (word) {
+        counts[word] = counts.hasOwnProperty(word) ? counts[word] + 1 : 1;
+    });
+
+    return counts;
+};
