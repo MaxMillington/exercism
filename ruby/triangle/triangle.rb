b9979497df4ab5da6207e74feb94af565d3a3077
MaxMillington <maxmillington89@gmail.com>
2015-08-06 11:37:20 -0600

    all tests passing on the triangle file

---
+++ ruby/triangle/triangle.rb
@@ -0,0 +1,38 @@
+class Triangle
+
+  def initialize(side1, side2, side3)
+    @sides = []
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides << @side1
+    @sides << @side2
+    @sides << @side3
+    @sides = @sides.group_by{|x| x}
+  end
+
+  def kind
+    if @sides.keys.first == 0
+      raise TriangleError
+    elsif @sides.keys.any? {|x| x < 0}
+      raise TriangleError
+    elsif @side1 + @side2 <= @side3
+      raise TriangleError
+    elsif @side2 + @side3 <= @side1
+      raise TriangleError
+    elsif @side3 + @side1 <= @side2
+      raise TriangleError
+    elsif @sides.size == 1
+      return :equilateral
+    elsif @sides.size == 2
+      return :isosceles
+    elsif @sides.size ==3
+      return :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
+end
