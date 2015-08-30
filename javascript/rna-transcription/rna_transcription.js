b4de11600c90bd7568edcb244414406709bc188f
MaxMillington <maxmillington89@gmail.com>
2015-08-29 19:05:53 -0600

    rna ran rana nra

---
+++ javascript/rna-transcription/rna_transcription.js
@@ -0,0 +1,15 @@
+module.exports = toRna;
+
+
+var dnaToRna = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U"
+};
+
+function toRna(dna) {
+    return dna.replace(/./g, function(dnaNucleotide) {
+        return dnaToRna[dnaNucleotide];
+    });
+}
